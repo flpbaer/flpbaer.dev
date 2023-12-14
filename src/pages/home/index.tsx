d882dd83ad618b955d18ed3c14b0fc9cb58a8e2f
Felipe <flpbaer@gmail.com>
2023-12-14 10:21:10 -0300

    finish portfolio

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,57 @@
-import { Description, StyledButton, Title } from "../../assets/styles/globalStyles";
-import { Container, DrawableTech, Image, Left, Right, TextContent } from "./style";
+import {
+  Description,
+  StyledButton,
+  Title,
+} from "../../assets/styles/globalStyles";
+import {
+  Container,
+  DrawableTech,
+  FloatIcon,
+  Image,
+  Left,
+  Right,
+  TextContent,
+} from "./style";
 import hand from "../../assets/images/👋.svg";
-import drawable from "../../assets/images/Drawable.png";
+import blur from "../../assets/images/blur.svg";
+import blur2 from "../../assets/images/blur2.svg";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import { BiLogoTypescript } from "react-icons/bi";
+import { FaReact } from "react-icons/fa";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 function Home() {
+  const infos = `const me = [{ name: "Felipe Baer", age: 17 }]
+
+const techs = [{ 
+    front: [
+     "html5", 
+     "css3", 
+     "javascript", 
+     "typescript", 
+     "react.js", 
+     "next.js", 
+     "node.js"
+     ],
+    back: ["java","node.js"],
+    infra: ["git(CI/CD - Pipeline)"],
+    cloud: ["aws"],
+    test: ["jest"],
+    pratices: [
+     "test driven development(tdd)",
+     "solid"
+     ]
+   }];
+
+const languages = [{ 
   
+    fluent: ["portuguese (pt-br)"],
+    intermediate: [
+     "english (eng)",
+     "spanish (es)",
+   ] 
+ }];
+  `;
+
   return (
     <Container>
       <Left>
@@ -19,36 +67,106 @@ function Home() {
             src={hand}
             initial={{ opacity: 0, x: -40 }}
             animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, delay: 0.4 }}
+            transition={{ duration: 1, delay: 0.8 }}
           />
         </TextContent>
-        <div style={{width: "60%"}}>
+        <div style={{ width: "60%" }}>
           <Description>
             Nice to meet you, I'm Felipe Bäer and I'm a software developer
             focused on front-end
           </Description>
         </div>
         <StyledButton>Download CV</StyledButton>
+        <img
+          src={blur}
+          style={{ position: "absolute", left: "0", bottom: "80px" }}
+        />
       </Left>
       <Right>
-        <DrawableTech></DrawableTech>
+        <FloatIcon
+          style={{ right: "80px", top: "110px", transform: "rotate(16deg)" }}
+          animate={["initial"]}
+          variants={{
+            grow: {
+              scale: 1.1,
+            },
+            rotate: {
+              rotate: [null, -5, 5, 0],
+              transition: {
+                duration: 25,
+              },
+            },
+            initial: {
+              y: [-20, 20],
+              rotate: 16,
+              transition: {
+                duration: 6,
+                repeat: Infinity,
+                // repeatDelay: 0.2,
+                repeatType: "reverse",
+              },
+            },
+          }}
+        >
+          <BiLogoTypescript size={100} style={{ color: "#017ACB" }} />
+        </FloatIcon>
+        <DrawableTech
+          initial={{ opacity: 0, y: 80 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.4 }}
+        >
+          <div style={{ padding: "20px", width: "100%" }}>
+            <span style={{ fontFamily: "Azeret Mono", fontSize: "18px" }}>
+              <SyntaxHighlighter
+                showLineNumbers
+                language="javascript"
+                style={atomOneDark}
+                customStyle={{ backgroundColor: "transparent" }}
+              >
+                {infos}
+              </SyntaxHighlighter>
+            </span>
+          </div>
+        </DrawableTech>
+        <FloatIcon
+          animate={["initial"]}
+          variants={{
+            grow: {
+              scale: 1.1,
+            },
+            rotate: {
+              rotate: [null, -5, 5, 0],
+              transition: {
+                duration: 15,
+              },
+            },
+            initial: {
+              y: [-20, 20],
+              rotate: -16,
+              transition: {
+                duration: 3,
+                repeat: Infinity,
+                // repeatDelay: 0.2,
+                repeatType: "reverse",
+              },
+            },
+          }}
+          style={{ top: "70%", left: "55%", transform: "rotate(-16deg)" }}
+        >
+          <FaReact size={100} style={{ color: "#61DAFB" }} />
+        </FloatIcon>
+        <img
+          src={blur2}
+          style={{
+            position: "absolute",
+            right: "20px",
+            top: "70px",
+            opacity: 0.7,
+          }}
+        />
       </Right>
     </Container>
   );
 }
 
 export default Home;
-
-// <Title>Hi!</Title>
-//               <img
-//                 src={hand}
-//                 style={{
-//                   marginLeft: "12px",
-//                 }}
-//               />
-//             </TitleContent>
-//             <DescriptionContent>
-//               <Description>
-//                 Nice to meet you, I'm Felipe Bäer and I'm a software developer
-//                 focused on front-end
-//               </Description>
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -26,14 +26,18 @@ export const Right = styled.div`
 flex: 1;
 padding: 20px;
 display: flex;
+justify-content: center;
 align-items: center;
 `;
 
 export const DrawableTech = styled(motion.div)`
-width: 520px;
+width: 650px;
 border-radius: 30px;
 min-height: 70%;
 max-height: 80%;
+display: flex;
+flex-direction: column;
+align-items: center;
 background: linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 12%);
 box-shadow: 0px -15px 24px 0px rgba(0, 0, 0, 0.10);
 `
@@ -41,3 +45,20 @@ box-shadow: 0px -15px 24px 0px rgba(0, 0, 0, 0.10);
 export const Image = styled(motion.img)`
 margin-left: 12px;
 `
+
+export const FloatIcon = styled(motion.div)`
+width: 100px;
+height: 100px;
+padding: 16px;
+background-color: #fff;
+z-index: 2;
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 30px;
+position: absolute;
+animation: float 4s ease-in infinite;
+box-shadow: 0px 1px 21.8px 0px rgba(0, 0, 0, 0.10);
+
+
+`;
